a936adbde63ac57532134a206175850245a35efa
JPampers <jp99@estudiantec.cr>
2022-10-03 18:39:30 -0600

    tesing response

--- nyt-api.py
+++ nyt-api.py
@@ -1,3 +1,9 @@
 import requests
+import csv
+import json
+
 
 response = requests.get("https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=lUgmzFgrUwNniZUP5adSLNTDIn2gAfst")
+info = response.json()
+resultsInfo = info['results']
+print(resultsInfo[2]['title'].lower())
