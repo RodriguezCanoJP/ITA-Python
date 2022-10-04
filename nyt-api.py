67af4f48f3d477792319f07eb0ddbecae1f814b4
JPampers <jp99@estudiantec.cr>
2022-10-03 19:40:07 -0600

    writes csv file

--- nyt-api.py
+++ nyt-api.py
@@ -5,5 +5,13 @@ import json
 
 response = requests.get("https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=lUgmzFgrUwNniZUP5adSLNTDIn2gAfst")
 info = response.json()
-resultsInfo = info['results']
-print(resultsInfo[2]['title'].lower())
+results = info['results']
+
+keys = [key for key in results[0].keys()]
+
+with open("output.csv", "w") as file:
+    csv_file = csv.writer(file, dialect='excel')
+    csv_file.writerow(keys)
+    for item in results:
+        csv_file.writerow([item[keys[0]], item[keys[1]], item[keys[2]], item[keys[3]], item[keys[4]], item[keys[5]], item[keys[6]], item[keys[7]], item[keys[8]], item[keys[9]], item[keys[10]]])
+
